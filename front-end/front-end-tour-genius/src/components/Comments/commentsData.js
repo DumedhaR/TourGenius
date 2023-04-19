88595575c61922dac8abcaa390505dea3077c016
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-04-19 18:47:41 +0530

    Create Comments Card

---
+++ front-end/front-end-tour-genius/src/components/Comments/commentsData.js
@@ -0,0 +1,10 @@
+// this file is removed when components are intergrated
+
+export const Commentsdata = [
+  {
+    name: 'Diluksha Shamal',
+    message:
+      'This is the best travel agency of choice, because of its topnotch service and value for money.',
+    profilePicture: 'images/avatar1.jpg'
+  }
+];
