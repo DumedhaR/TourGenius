1ffcf1152880819a0f36e325cad268248ca5ea90
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-04-23 16:02:52 +0530

    Add Rating for Destination Card

--- front-end/front-end-tour-genius/src/components/Destination/destinationData.js
+++ front-end/front-end-tour-genius/src/components/Destination/destinationData.js
@@ -6,6 +6,7 @@ export const DestinationData = [
     destinationCountry: 'Sri Lanka',
     destinationImage: 'images/destination1.png',
     destinationDescription:
-      'Sri Lanka is well known for its rich Buddhist culture as well as other religions. Being a religious country, Sri Lanka has many places with religious and historic significance, which attract tourists from all over the world.'
+      'Sri Lanka is well known for its rich Buddhist culture as well as other religions. Being a religious country, Sri Lanka has many places with religious and historic significance, which attract tourists from all over the world.',
+    destinationRating: 3.5
   }
 ];
