3781de15f4241ffde70f0417b5258353c6c07502
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-04-26 13:03:27 +0530

    Create Hotel Card

---
+++ front-end/front-end-tour-genius/src/components/Hotel/hotelData.js
@@ -0,0 +1,13 @@
+// this file is removed when components are intergrated
+
+export const HotelData = [
+  {
+    hotelName: 'Yala Hotel',
+    hotelCountry: 'Sri Lanka',
+    hotelDestination: 'Yala',
+    hotelImage: 'images/hotel1.jpeg',
+    hotelDescription:
+      'The car parking and the Wi-Fi are always free, so you can stay in touch and come and go as you please. Conveniently situated in the Yala Tissamaharama part of Yala, this property puts you close to attractions and interesting dining options. Rated with 5 stars, this high-quality property provides guests with access to massage, restaurant and hot tub on-site.',
+    hotelRating: 3.5
+  }
+];
