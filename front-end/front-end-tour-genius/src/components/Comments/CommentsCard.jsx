4f58d3949e0223e234cd1982c9397d9598ad43a4
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-05-10 11:53:52 +0530

    Fix issues of Comments Card

--- front-end/front-end-tour-genius/src/components/Comments/CommentsCard.jsx
+++ front-end/front-end-tour-genius/src/components/Comments/CommentsCard.jsx
@@ -3,19 +3,15 @@ import '../../utils/commentsCard.css';
 import quotation from '../../assests/images/quotation.png';
 import Avatar from '@mui/material/Avatar';
 
-function CommentsCard({ data }) {
+function CommentsCard({ name, message, profilePicture }) {
   return (
-    <div>
-      {data.map((element, index) => (
-        <div className="card" key={`card-${index}`}>
-          <img src={quotation} width="40px" height="40px" />
-          <div className="message">{element.message}</div>
-          <div className="user">
-            <Avatar alt={element.name} src={element.profilePicture} />
-            <div className="userName">{element.name}</div>
-          </div>
-        </div>
-      ))}
+    <div className="cardComments">
+      <img src={quotation} width="40px" height="40px" />
+      <div className="messageComments">{message}</div>
+      <div className="userComments">
+        <Avatar alt={name} src={profilePicture} />
+        <div className="userNameComments">{name}</div>
+      </div>
     </div>
   );
 }
