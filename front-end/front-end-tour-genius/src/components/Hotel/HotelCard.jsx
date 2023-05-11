5df8d064006be730624a5e0755e023f649ed574c
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-05-11 11:47:27 +0530

    Fix Hotel card issues

--- front-end/front-end-tour-genius/src/components/Hotel/HotelCard.jsx
+++ front-end/front-end-tour-genius/src/components/Hotel/HotelCard.jsx
@@ -9,7 +9,14 @@ import Rating from '@mui/material/Rating';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function HotelCard({ data }) {
+function HotelCard({
+  hotelName,
+  hotelImage,
+  hotelRating,
+  hotelCountry,
+  hotelDestination,
+  hotelDescription
+}) {
   const [showMore, setShowMore] = useState(false);
   const navigate = useNavigate();
 
@@ -17,56 +24,50 @@ function HotelCard({ data }) {
     navigate();
   };
   return (
-    <div>
-      {data.map((element, index) => (
-        <div className="card" key={`hotelCard-${index}`}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt={element.hotelName}
-              height="200px"
-              image={element.hotelImage}
-              onClick={navigateToHotel}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {element.hotelName}
-              </Typography>
-              <div className="rating" style={{ display: 'flex' }}>
-                <Rating name="read-only" value={element.hotelRating} readOnly />
-                <Typography
-                  sx={{ marginLeft: '10px', marginTop: '-5px' }}
-                  gutterBottom
-                  variant="h6"
-                  component="div">
-                  {element.hotelRating}
-                </Typography>
-              </div>
-              <Typography gutterBottom variant="h7" component="div">
-                Hotel in{' '}
-                <b>
-                  {element.hotelDestination} {element.hotelCountry}
-                </b>
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {showMore
-                  ? element.hotelDescription
-                  : element.hotelDescription && element.hotelDescription.substring(0, 100)}
-              </Typography>
-            </CardContent>
-            {element.hotelDescription.length >= 100 && (
-              <CardActions>
-                <Button size="small" onClick={() => setShowMore(!showMore)}>
-                  {!showMore ? 'show more' : 'show less'}
-                </Button>
-                <Button size="small" onClick={navigateToHotel}>
-                  visit
-                </Button>
-              </CardActions>
-            )}
-          </Card>
-        </div>
-      ))}
+    <div className="cardHotel">
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt={hotelName}
+          height="200px"
+          image={hotelImage}
+          onClick={navigateToHotel}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {hotelName}
+          </Typography>
+          <div className="rating" style={{ display: 'flex' }}>
+            <Rating name="read-only" value={hotelRating} readOnly />
+            <Typography
+              sx={{ marginLeft: '10px', marginTop: '-5px' }}
+              gutterBottom
+              variant="h6"
+              component="div">
+              {hotelRating}
+            </Typography>
+          </div>
+          <Typography gutterBottom variant="h7" component="div">
+            Hotel in{' '}
+            <b>
+              {hotelDestination} {hotelCountry}
+            </b>
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {showMore ? hotelDescription : hotelDescription && hotelDescription.substring(0, 100)}
+          </Typography>
+        </CardContent>
+        {hotelDescription.length >= 100 && (
+          <CardActions>
+            <Button size="small" onClick={() => setShowMore(!showMore)}>
+              {!showMore ? 'show more' : 'show less'}
+            </Button>
+            <Button size="small" onClick={navigateToHotel}>
+              visit
+            </Button>
+          </CardActions>
+        )}
+      </Card>
     </div>
   );
 }
