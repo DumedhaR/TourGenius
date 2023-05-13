9d2cd227c1a4f2d96cdab2e21b4f024562616b8d
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-05-13 15:37:15 +0530

    Create ImageList Component

---
+++ front-end/front-end-tour-genius/src/components/ImageList/imageData.js
@@ -0,0 +1,52 @@
+// this file is removed when components are intergrated
+
+export const imageData = [
+  {
+    destinationImageList: 'images/destinationImages/yala1.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala2.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala3.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala4.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala5.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala6.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala7.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala8.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala9.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala10.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala11.jpg',
+    destinationName: 'Yala'
+  },
+  {
+    destinationImageList: 'images/destinationImages/yala12.jpg',
+    destinationName: 'Yala'
+  }
+];
