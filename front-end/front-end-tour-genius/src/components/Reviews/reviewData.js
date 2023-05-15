895c95f8a39c32ab51a6dc55ac4e203c96f66c56
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-05-15 13:20:02 +0530

    Create ReviewCard

---
+++ front-end/front-end-tour-genius/src/components/Reviews/reviewData.js
@@ -0,0 +1,33 @@
+// this file is removed when components are intergrated
+
+export const ReviewData = [
+  {
+    name: 'Shamal',
+    country: 'Sri Lanka',
+    profilePicture: 'images/avatar1.jpg',
+    rating: 2.5,
+    userMessage: 'Ko kiwwa tharam saththu nane ???'
+  },
+  {
+    name: 'Sandaruwan',
+    country: 'Sri Lanka',
+    profilePicture: 'images/avatar2.jpg',
+    rating: 2,
+    userMessage: 'Hey guys, Yala Park is one of the best places I have evertraveled in my life.'
+  },
+  {
+    name: 'Rukshan',
+    country: 'England',
+    profilePicture: 'images/avatar3.jpg',
+    rating: 5,
+    userMessage:
+      'Hey guys, Yala Park is one of the best places I have evertraveled in my life. But kiwwa tharam saththu na'
+  },
+  {
+    name: 'Thamara',
+    country: 'India',
+    profilePicture: 'images/avatar1.jpg',
+    rating: 4,
+    userMessage: 'Hey guys, Yala Park is one of the best places I have evertraveled in my life.'
+  }
+];
