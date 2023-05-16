b752be73b48c806b2ce6117c2515834bf86f70f9
Rukshan Perera <pererarukshan45@gmail.com>
2023-05-16 13:38:40 +0530

    Renamed UserService to TravelerService

--- back-end/TravellerService/src/main/java/com/tourgenius/travellerservice/controller/TravellerController.java
+++ back-end/TravellerService/src/main/java/com/tourgenius/travellerservice/controller/TravellerController.java
@@ -9,25 +9,30 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/travellers")
 @RequiredArgsConstructor
 public class TravellerController {
 
     private final TravellerService travellerService;
 
     @PostMapping("/create")
-    public Traveller createUser(@RequestBody TravellerDto travellerDto) throws ParseException {
+    public Traveller createTraveller(@RequestBody TravellerDto travellerDto) throws ParseException {
         return travellerService.createTraveller(travellerDto);
     }
 
     @PutMapping("/update")
-    public Traveller updateUser(@RequestBody TravellerDto travellerDto) throws ParseException {
+    public Traveller updateTraveller(@RequestBody TravellerDto travellerDto) throws ParseException {
         return travellerService.updateTraveller(travellerDto);
     }
 
     @DeleteMapping("/delete/{email}")
-    public boolean deleteUser(@PathVariable String email){
+    public boolean deleteTraveller(@PathVariable String email){
         return travellerService.deleteTraveller(email);
+    }
+
+    @GetMapping("/{email}")
+    public Traveller getTraveller(@PathVariable String email){
+        return travellerService.getTraveller(email);
     }
 
 }
--- back-end/TravellerService/src/main/java/com/tourgenius/travellerservice/repository/TravellerRepository.java
+++ back-end/TravellerService/src/main/java/com/tourgenius/travellerservice/repository/TravellerRepository.java
@@ -2,7 +2,9 @@ package com.tourgenius.travellerservice.repository;
 
 import com.tourgenius.travellerservice.model.Traveller;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TravellerRepository extends MongoRepository<Traveller, String> {
 
     Traveller findTravellerByEmail(String email);
