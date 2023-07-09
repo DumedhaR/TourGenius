c0e314192e4e67ae1b07544e8f3959c94dd85d85
latdliyanaarachchi <latdliyanaarachchi@students.nsbm.ac.lk>
2023-07-09 15:28:37 +0530

    Added changes to Booking service

--- back-end/RoomReservationService/src/main/java/com/codemind/tourgenius/controller/ClientPackageController.java
+++ back-end/RoomReservationService/src/main/java/com/codemind/tourgenius/controller/ClientPackageController.java
@@ -1,20 +1,14 @@
 package com.codemind.tourgenius.controller;
 
-import com.codemind.tourgenius.dto.BookingResultDto;
-import com.codemind.tourgenius.dto.request.BookingRequest;
 import com.codemind.tourgenius.dto.request.PackageRequest;
 import com.codemind.tourgenius.dto.request.RoomSearchRequest;
 import com.codemind.tourgenius.dto.response.RoomSearchResponse;
-import com.codemind.tourgenius.model.Booking;
-import com.codemind.tourgenius.model.Room;
 import com.codemind.tourgenius.repository.BookingRepository;
 import com.codemind.tourgenius.service.ClientPackageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/package")
@@ -23,16 +17,14 @@ public class ClientPackageController {
     @Autowired
     private ClientPackageService clientPackageService;
 
-    @Autowired
-    private BookingRepository bookingRepository;
 
     @PostMapping("/create")
-    public String createClientPackage(@RequestBody PackageRequest request){
+    public String createClientPackage(@RequestBody PackageRequest request) {
         return clientPackageService.create(request);
     }
 
-    @PutMapping ("/update/{id}")
-    public String updateClientPackage(@PathVariable String id,@RequestBody PackageRequest request){
+    @PutMapping("/update/{id}")
+    public String updateClientPackage(@PathVariable String id, @RequestBody PackageRequest request) {
         return clientPackageService.update(id, request);
     }
 
@@ -41,7 +33,7 @@ public class ClientPackageController {
         return clientPackageService.search(roomSearchRequest);
     }
 
-    @DeleteMapping ("delete/{id}")
+    @DeleteMapping("delete/{id}")
     public String deleteClientPackage(@PathVariable String id) {
         return clientPackageService.delete(id);
     }
